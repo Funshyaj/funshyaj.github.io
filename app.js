03dffb302f7c6d3699b3817d96433f8266c0a2a9
Funshyaj <funshoJyi29@gmail.com>
2023-02-11 15:38:42 +0100

    Added web application section and see more button with full functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'); const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul'); const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'); const header = document.querySelector('.header.container'); hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active');
-}); document.addEventListener('scroll', () => {
+}); 
+document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY; if (scroll_position > 250) {
         header.style.backgroundColor = '#29323c';
     } else {
@@ -10,3 +11,22 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
         hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active');
     });
 });
+
+
+// logic for see more button on projects
+const seeMoreBtn = document.querySelectorAll('.see-more-button');
+
+seeMoreBtn.forEach(btn => {
+   btn.addEventListener('click', () => {
+let seeMore = btn.nextElementSibling;
+if (seeMore.style.display === 'block'){
+   btn.innerHTML = 'See more...'
+    seeMore.style.display = 'none'
+    btn.style.paddingBottom = '0px'
+    
+}
+ else seeMore.style.display = 'block' , btn.innerHTML = 'See less...' ,  btn.style.paddingBottom = '10px'
+
+})
+})
+    
